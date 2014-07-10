f5692970d1f1f33d1f0f8208f9b716f2d07f9f7b
ThirteenAG <thirteenag@gmail.com>
2014-07-10 16:01:33 +0400

    vc10 small fixes

--- src/translator/translator.cpp
+++ src/translator/translator.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * Mod Loader - Address Translation Between Game Versions
  * Copyright (C) 2013-2014  LINK/2012 <dma_2012@hotmail.com>
  * Licensed under the MIT License, see LICENSE at top level directory.
@@ -8,7 +8,7 @@
 #include <modloader/util/injector.hpp>
 #include <map>
 using namespace injector;
-
+ 
 // Tables
 #include "gta3/sa/10us.hpp"
 #include "gta3/sa/10eu.hpp"
@@ -18,8 +18,8 @@ using namespace injector;
 // Constants
 static const size_t max_ptr_dist = 8;       // Max distance to take as a "equivalent" address for modloader
 static void init(std::map<memory_pointer_raw, memory_pointer_raw>& map);
-
-
+ 
+ 
 // Translate pointer from GTA SA 10US offset to this executable offset
 void* injector::address_manager::translator(void* p_)
 {
@@ -28,22 +28,22 @@ void* injector::address_manager::translator(void* p_)
     //return p_;
     memory_pointer_raw p = p_;
     memory_pointer_raw result = nullptr;
-                
+               
     // Initialize if hasn't initialized yet
     init(map);
-
+ 
     // Find first element in the map that is greater than or equal to p
     auto it = map.lower_bound(p);
     if(it != map.end())
     {
         // If it's not exactly the address, get back one position on the table
         if(it->first != p) --it;
-
+ 
         auto diff = uintptr_t(p - it->first);       // What's the difference between p and that address?
         if(diff <= max_ptr_dist)                    // Could we live with this difference in hands?
             result = it->second + raw_ptr(diff);    // Yes, we can!
     }
-    
+   
     // If we couldn't translate the address, notify
     if(!result)
     {
@@ -57,10 +57,10 @@ void* injector::address_manager::translator(void* p_)
         else if(true) MessageBoxA(0, buf, injector::game_version_manager::PluginName, 0);
 #endif
     }
-    
+   
     return result.get();
 }
-
+ 
 // Initializes the address translator and it's table
 static void init(std::map<memory_pointer_raw, memory_pointer_raw>& map)
 {
@@ -69,12 +69,12 @@ static void init(std::map<memory_pointer_raw, memory_pointer_raw>& map)
     {
         auto& gvm = injector::address_manager::singleton();
         bInitialized = true;
-        
+       
         // The map must have null pointers at it's bounds
         // So they work properly with lower_bound and stuff
         map.emplace(0x00000000u, 0x00000000u);
         map.emplace(0xffffffffu, 0xffffffffu);
-
+ 
         // We're only working with SA addresses on here
         if(gvm.IsSA())
         {
