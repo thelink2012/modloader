fe6d57cd1595099606aa973357f8352eda00af3d
Mário Feroldi <thelost-t@live.com>
2015-02-16 13:29:05 -0200

    Optimized relative_epsilon_checker
    Remade a chunk of relative_epsilon_checker body to be more abstract.

--- src/shared/type_wrapper/floating_point.hpp
+++ src/shared/type_wrapper/floating_point.hpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2014 Denilson das Mercês Amorim (aka LINK/2012)
+ *  Copyright (C) 2014 Denilson das MercÃªs Amorim (aka LINK/2012)
  *  Licensed under the Boost Software License v1.0 (http://opensource.org/licenses/BSL-1.0)
  *
  */
@@ -40,25 +40,6 @@ struct floating_point_comparer
         // Internal stuff
         struct detail
         {
-            // Performs the comparision between 'a' and 'b' using their 'delta' by using relative epsilon comparision
-            // @Pred is the predicate to compare the delta with the epsilons
-            // @Ep is the epsilon values
-            template<class Pred, class Ep>
-            struct relative_epsilon_checker
-            {
-                template<class T>
-                bool operator()(const T& delta, const T& a, const T& b) const
-                {
-                    Pred pred;
-                    if(pred(delta, Ep::absolute_epsilon())) // For numbers near zero
-                        return true;
-                    else if(pred(delta, ((std::max)(std::abs(a), std::abs(b)) * Ep::relative_epsilon())))   // For numbers farther from zero
-                        return true;
-                    else
-                        return false;
-                }
-            };
-
             // Performs the comparision between 'a' and 'b' using their 'delta' by using absolute/fixed epsilon comparision
             // @Pred is the predicate to compare the delta with the epsilons
             // @Ep is the epsilon values
@@ -73,6 +54,21 @@ struct floating_point_comparer
                         return true;
                     else
                         return false;
+                }
+            };
+            
+            // Performs the comparision between 'a' and 'b' using their 'delta' by using relative epsilon comparision
+            // @Pred is the predicate to compare the delta with the epsilons
+            // @Ep is the epsilon values
+            template<class Pred, class Ep>
+            struct relative_epsilon_checker
+            {
+                template<class T>
+                bool operator()(const T& delta, const T& a, const T& b) const
+                {
+                    Pred pred;
+                    return absolute_epsilon_checker<Pred, Ep>()(delta, a, b) ? // For numbers near zero
+                        true : pred(delta, ((std::max)(std::abs(a), std::abs(b)) * Ep::relative_epsilon()))); // For numbers farther from zero
                 }
             };
 
