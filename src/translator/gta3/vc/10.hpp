f8fb3b5fc75d8eb3f85663031339ca5ff206252f
Denilson M. Amorim <dma_2012@hotmail.com>
2016-06-17 21:56:56 -0300

    Some reorganization on the addr tables

--- src/translator/gta3/vc/10.hpp
+++ src/translator/gta3/vc/10.hpp
@@ -235,70 +235,26 @@ static void vc_10(std::map<memory_pointer_raw, memory_pointer_raw>& map)
         map[0x464D50] = 0x44FE30;   // _ZN11CTheScripts18IsPlayerOnAMissionEv
         map[0x5B6890] = 0x578CC0;   // _ZN17CVehicleModelInfo18LoadVehicleColoursEv
         map[0x5B68AB] = 0x578CE3;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CVehicleModelInfo::LoadVehicleColours
-        //map[0x5B65A0] = ;   // _ZN17CVehicleModelInfo19LoadVehicleUpgradesEv //doesn't exist
-        //map[0x5B65BE] = ;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CVehicleModelInfo::LoadVehicleUpgrades //doesn't exist
-        //map[0xB4E6D8] = ;   // CLinkedUpgradeList CVehicleModelInfo::ms_linkedUpgradesE //doesn't exist
-        map[0x4C6770] = 0x55F5D0;   // mov     eax, gsVehicleModels.m_nCount //=> cmp     ds:dwLoadedVehicleModels, 6Eh doublecheck!
         map[0x5B905E] = 0x48D97C;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CFileLoader::LoadLevel
         map[0x5BD830] = 0x5AAE20;   // _ZN16cHandlingDataMgr16LoadHandlingDataEv
-        //map[0x5BD850] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @cHandlingDataMgr::LoadHandlingData //check 5AAE4E
-        map[0x5BD850] = 0x5AAE4E; // call    _ZN8CFileMgr12LoadTextFileEPKcPhi ; "handling.cfg"
+        map[0x5BD850] = 0x5AAE4E;   // call    _ZN8CFileMgr12LoadTextFileEPKcPhi        ; @cHandlingDataMgr::LoadHandlingData
         map[0xC2B9C8] = 0x978E58;   // mod_HandlingManager CHandlingData
         map[0x5B8428] = 0x48C846;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CFileLoader::LoadObjectTypes
         map[0x5B871A] = 0x48B079;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CFileLoader::LoadScene
-        //map[0x5DD780] = 0x;   // _ZN9CPlantMgr12ReloadConfigEv //doesn't exist?
-        //map[0x5DD3D1] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CPlantSurfPropMgr::LoadPlantsDat //doesn't exist?
         map[0x5BF750] = 0x5D5750;   // _ZN11CWeaponInfo10InitialiseEv
-        //map[0x5BE68A] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CWeaponInfo::Initialise // check 5D527B
-        map[0x5BE68A] = 0x5D527B; // call    _ZN8CFileMgr12LoadTextFileEPKcPhi             ; @CWeaponInfo::Initialise
-        //map[0x5BC090] = 0x;   // _ZN9CPopCycle10InitialiseEv //doesn't exist?
-        //map[0x5BC0AE] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CPopCycle::Initialise //doesn't exist?
+        map[0x5BE68A] = 0x5D527B;   // call    _ZN8CFileMgr12LoadTextFileEPKcPhi        ; @CWeaponInfo::Initialise
         map[0x5BBAC0] = 0x4D05E0;   // _ZN10CTimeCycle10InitialiseEb
-        //map[0x5BBADE] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CTimeCycle::initialise //check 4D0614
+        map[xVc(0x4D0614)] = 0x4D0614; // call    _ZN8CFileMgr12LoadTextFileEPKcPhi     ; @CTimeCycle::Initialise
         map[0x6EAE80] = 0x5C3940;   // _ZN11CWaterLevel20WaterLevelInitialiseEv
-        //map[0x5A7B30] = 0x;   // _ZN8CClothes15LoadClothesFileEv  //doesn't exist?
-        //map[0x5A7B54] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CClothes::LoadClothesFile   //doesn't exist?
-        //map[0x5C0297] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @FurnitureManager_c::LoadFurniture   //doesn't exist?
         map[0x6EAF4D] = 0x5C395A;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CWaterLevel::WaterLevelInitialise
-        //map[0x5BD1A0] = 0x;   // _ZN11CPopulation13LoadCarGroupsEv //doesn't exist?
-        //map[0x5BD1BB] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CPopulation::LoadCarGroups //doesn't exist?
         map[0x5BCFE0] = 0x53E9C0;   // _ZN11CPopulation13LoadPedGroupsEv
         map[0x5BCFFB] = 0x53E9DF;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CPopulation::LoadPedGroups
         map[0x5BB890] = 0x530020;   // _ZN9CPedStats12LoadPedStatsEv
-        //map[0x5BB89F] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CPedStats::LoadPedStats //check 530054
-        map[0x5BB89F] = 0x530054;   // call    _ZN8CFileMgr12LoadTextFileEPKcPhi             ; @CPedStats::LoadPedStats
+        map[0x5BB89F] = 0x530054;   // call    _ZN8CFileMgr12LoadTextFileEPKcPhi        ; @CPedStats::LoadPedStats
         map[0x5B5360] = 0x4E4560;   // _ZN11CObjectData10InitialiseEPcb
-        //map[0x5B5444] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CObjectData::Initialise // check 4E476D
-        map[0x5B5444] = 0x4E476D;   // call    _ZN8CFileMgr12LoadTextFileEPKcPhi          ; @CObjectData::Initialise
-        //map[0x7187C0] = 0x;   // _ZN5CFont14LoadFontValuesEv //doesn't exist?
-        //map[0x7187DB] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CFont::LoadFontValues //doesn't exist?
-        map[0x461100] = 0x444AA0;   // _ZN11CRoadBlocks4InitEv
-        //map[0x461125] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CRoadBlocks::Init //doesn't exist
-        //map[0x5599B0] = 0x;   // _ZN6CStats23LoadActionReactionStatsEv //doesn't exist
-        //map[0x5599D8] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CStats::LoadActionReactionStats //doesn't exist?
-        //map[0x55988F] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CStats::LoadStatUpdateConditions //doesn't exist?
-        //map[0x608B45] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CPedType::LoadPedData //check 530BD7
-        map[0x608B45] = 0x530BD7; // call    _ZN8CFileMgr12LoadTextFileEPKcPhi ; @CPedType::LoadPedData
-        //map[0x5BEDC0] = 0x;   // _ZN16CTaskSimpleFight13LoadMeleeDataEv //doesn't exist
-        //map[0x5BEF47] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CTaskSimpleFight::LoadMeleeData //doesn't exist
-        //map[0x5BC92B] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CAnimManager::ReadAnimAssociationDefinitions //doesn't exist?
-        //map[0x5BF400] = 0x;   //  _ZN29CDecisionMakerTypesFileLoader24LoadDefaultDecisionMakerEv //doesn't exist?
-        //map[0x6076CE] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CDecisionMakerTypesFileLoader::LoadDecisionMaker //doesn't exist?
-        //map[0x5A3EA0] = 0x;   // _ZN15ProcObjectMan_c4InitEv //doesn't exist
-        //map[0xBB7CB0] = 0x;   // ProcObjectMan g_procObjMan //doesn't exist
-        //map[0x5A3154] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @ProcObjectMan_c::LoadDataFile //doesn't exist
-        //map[0x55F420] = 0x;   // _ZN14SurfaceInfos_c4InitEv //not sure, check 4CE8A0
-        //map[0xB79538] = 0x;   // SurfaceInfos_c g_surfaceInfos //^
-        //map[0x55D100] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @SurfaceInfos_c::LoadAdhesiveLimits//^
-        //map[0x55EBA4] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @SurfaceInfos_c::LoadSurfaceInfos//^
-        //map[0x55F2C1] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @SurfaceInfos_c::LoadSurfaceAudioInfos//^
-        //map[0x49AE30] = 0x;   // _ZN9CShopping16RemoveLoadedShopEv //doesn't exist
-        //map[0x49BBE0] = 0x;   // _ZN9CShopping8LoadShopEPKc //doesn't exist
-        //map[0xA9A7D8] = 0x;   // char _ZN9CShopping13ms_shopLoadedE[24] //doesn't exist
-        //map[0x49B6AF] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CShopping::LoadStats //doesn't exist
-        //map[0x49B93F] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CShopping::LoadPrices //doesn't exist
-        //map[0x49BC98] = 0x;   // call    _ZN8CFileMgr8OpenFileEPKcS1_             ; @CShopping::LoadShop //doesn't exist
-        map[0x6F7440] = 0x5B2CA0;   // _ZN6CTrain10InitTrainsEv
+        map[0x5B5444] = 0x4E476D;   // call    _ZN8CFileMgr12LoadTextFileEPKcPhi        ; @CObjectData::Initialise
+        map[0x608B45] = 0x530BD7; // call    _ZN8CFileMgr12LoadTextFileEPKcPhi          ; @CPedType::LoadPedData
+        //map[0x6F7440] = 0x5B2CA0;   // _ZN6CTrain10InitTrainsEv //doesn't exist -- just a nullsub
         //map[0x6F7470] = 0x;   // call    _ZN6CTrain25ReadAndInterpretTrackFileEPcPP10CTrainNodePiPfi ; "tracks.dat" //doesn't exist
         //map[0x6F74BC] = 0x;   // call    _ZN6CTrain25ReadAndInterpretTrackFileEPcPP10CTrainNodePiPfi ; "tracks2.dat" //doesn't exist
         //map[0x6F7496] = 0x;   // call    _ZN6CTrain25ReadAndInterpretTrackFileEPcPP10CTrainNodePiPfi ; "tracks3.dat" //doesn't exist
@@ -314,14 +270,13 @@ static void vc_10(std::map<memory_pointer_raw, memory_pointer_raw>& map)
         map[0x4C5940] = 0x55F7D0;   // _ZN10CModelInfo12GetModelInfoEPKcPi
         map[0x5B925F] = 0x48DCF2;   // call    _ZN11CObjectData10InitialiseEPc          ; @CFileLoader::LoadLevel
 
-        map[xVc(0x5C395A)] = 0x5C395A; // call    _ZN8CFileMgr8OpenFileEPKcS1_ ; "data/waterpro.dat"
-        map[xVc(0x4D0614)] = 0x4D0614; // call    _ZN8CFileMgr12LoadTextFileEPKcPhi ; "timecyc.dat"
+        map[xVc(0x5C395A)] = 0x5C395A; // call    _ZN8CFileMgr8OpenFileEPKcS1_          ; "data/waterpro.dat"
         map[xVc(0x4CE8A0)] = 0x4CE8A0; // _ZN13CSurfaceTable10InitialiseEPc
-        map[0x55D100] = 0x4CE8CC; // call    _ZN8CFileMgr12LoadTextFileEPKcPhi
-        map[xVc(0x565B2E)] = 0x565B2E; // call    _ZN8CFileMgr12LoadTextFileEPKcPhi
+        map[0x55D100] = 0x4CE8CC; // call    _ZN8CFileMgr12LoadTextFileEPKcPhi          ; @CSurfaceTable::Initialise
         map[xVc(0x565940)] = 0x565940; // _ZN9CParticle12ReloadConfigEv
-        map[xVc(0x527590)] = 0x527590; // call    _ZN8CFileMgr12LoadTextFileEPKcPhi
+        map[xVc(0x565B2E)] = 0x565B2E; // call    _ZN8CFileMgr12LoadTextFileEPKcPhi     ; @cParticleSystemMgr::LoadParticleData
         map[xVc(0x527570)] = 0x527570; // _ZN4CPed13LoadFightDataEv
+        map[xVc(0x527590)] = 0x527590; // call    _ZN8CFileMgr12LoadTextFileEPKcPhi     ; @CPed::LoadFightData
     }
 
     // traits
@@ -331,12 +286,6 @@ static void vc_10(std::map<memory_pointer_raw, memory_pointer_raw>& map)
         map[0x5B6314] = 0x40FD1B;   // -> DWORD 61A8h   ; COL Start Index
         map[0x5B63C5] = 0x40FD48;   // -> DWORD 63E7h   ; IFP Start Index
         map[0x408897] = 0x4014EF+1; // -> offset _ZN10CModelInfo16ms_modelInfoPtrsE
-
-
-        // TODO FIXME LATER
-        static uint32_t minusone32 = -1;
-        map[0x5B63F1] = (uintptr_t)&minusone32;
-        map[0x5B641F] = (uintptr_t)&minusone32;
     }
  
     // helpers for VC emulating SA
