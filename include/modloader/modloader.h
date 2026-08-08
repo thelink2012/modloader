c44a5bdbf2e56223ca0023f25d4d2d1d996ec9b5
Denilson Amorim <denimorim@gmail.com>
2026-08-08 15:08:51 -0300

    Bump version to 0.3.11 for dev builds

--- include/modloader/modloader.h
+++ include/modloader/modloader.h
@@ -29,7 +29,7 @@ extern "C" {
 /* Version */
 #define MODLOADER_VERSION_MAJOR         0
 #define MODLOADER_VERSION_MINOR         3
-#define MODLOADER_VERSION_REVISION      10
+#define MODLOADER_VERSION_REVISION      11
 #ifdef MODLOADER_FINAL_RELEASE
 #define MODLOADER_VERSION_ISDEV         0
 #else
